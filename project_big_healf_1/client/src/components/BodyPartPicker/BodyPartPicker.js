d55cdb935fe27803226c6b2e84e6c23d8791652d
SashaZ <sasha.zahnitko@gmail.com>
2021-01-04 08:14:00 +1000

    Some changes to the structure of the body parts.

--- project_big_healf_1/client/src/components/BodyPartPicker/BodyPartPicker.js
+++ project_big_healf_1/client/src/components/BodyPartPicker/BodyPartPicker.js
@@ -7,7 +7,6 @@ const BodyPartPicker = () => {
   return (
     <Wrapper>
       <div>Body Part Picker</div>
-      <pre>{JSON.stringify({ parts: Object.entries(bodyParts) }, null, 2)}</pre>
     </Wrapper>
   );
 };
